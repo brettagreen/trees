1db6b2f13545afed0d28099edc8fcc3ecb6abde7
Brett Green <brettalangreen@gmail.com>
2023-08-12 15:57:06 -0500

    trees and binary trees exercises

--- binary-tree.js
+++ binary-tree.js
@@ -25,23 +25,23 @@ class BinaryTree {
            return 0;
         }
 
-        function ridiculous(node, count) {
+        function gleanDepth(node, count) {
 
             if (!node.left || !node.right) {
                 return count;
             } else {
                 count++;
-                subLeftCount = ridiculous(node.left, count);
+                subLeftCount = gleanDepth(node.left, count);
                 console.log('slc', subLeftCount);
-                subRightCount = ridiculous(node.right, count);
+                subRightCount = gleanDepth(node.right, count);
                 console.log('src', subRightCount);
             }
 
             return subLeftCount < subRightCount ? subLeftCount : subRightCount;
         }
 
-        const leftCount = ridiculous(node.left, 1);
-        const rightCount = ridiculous(node.right, 1);
+        const leftCount = gleanDepth(node.left, 1);
+        const rightCount = gleanDepth(node.right, 1);
 
         return leftCount > rightCount ? rightCount : leftCount;
 
@@ -51,13 +51,89 @@ class BinaryTree {
      * the length of the longest path from the root to a leaf. */
 
     maxDepth() {
+        let node = this.root;
+        let leftCount = 0;
+        let rightCount = 0;
+        let subLeftCount;
+        let subRightCount;
 
+        if (!node.left && !node.right) {
+           return 0;
+        }
+
+        function gleanDepth(node, count) {
+
+            if (!node.left && !node.right) {
+                return count;
+            } else {
+                count++;
+                if (node.left) {
+                    subLeftCount = gleanDepth(node.left, count);
+                    console.log('slc', subLeftCount);
+                }
+                if (node.right) {
+                    subRightCount = gleanDepth(node.right, count);
+                    console.log('src', subRightCount);
+                }
+
+            }
+
+            return subLeftCount > subRightCount ? subLeftCount : subRightCount;
+        }
+
+        if (node.left) {
+            leftCount = gleanDepth(node.left, 1);
+        }
+        if (node.right) {
+            rightCount = gleanDepth(node.right, 1);
+        }
+
+        return leftCount > rightCount ? leftCount : rightCount;
     }
 
     /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
      * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
     maxSum() {
+        let node = this.root;
+        let leftSum = 0;
+        let rightSum = 0;
+        let subLeftSum;
+        let subRightSum;
+
+        if (!node.left && !node.right) {
+           return node.val ? node.val : 0;
+        }
+
+        function gleanVal(node, sum) {
+
+            if (!node.left && !node.right) {
+                let val = node.val ? node.val : 0;
+                return sum + val;
+            } else {
+                if (node.left) {
+                    let val = node.val ? node.val : 0;
+                    let subSum = sum + val;
+                    subLeftSum = gleanVal(node.left, subSum);
+                }
+                if (node.right) {
+                    let val = node.val ? node.val : 0;
+                    let subSum = sum + val;
+                    subRightSum = gleanVal(node.right, subSum);
+                }
+
+            }
+            return subLeftSum > subRightSum ? subLeftSum : subRightSum;
+        }
+
+        if (node.left) {
+            leftSum = gleanVal(node.left, node.val ? node.val : 0);
+        }
+        if (node.right) {
+            rightSum = gleanVal(node.right, node.val ? node.val : 0);
+        }
+
+        return leftSum > rightSum ? leftSum : rightSum;
 
     }
 
@@ -65,6 +141,26 @@ class BinaryTree {
      * which is larger than lowerBound. Return null if no such value exists. */
 
     nextLarger(lowerBound) {
+        let stack = [this.root];
+        let lowest = null;
+
+        while (stack.length) {
+			let current = stack.pop();
+
+			if (current.val > lowerBound) {
+                if (!lowest) {
+                    lowest = current.val;
+                } else if (current.val < lowest) {
+                    lowest = current.val;
+                }
+			}
+            
+            if (current.left) stack.push(current.left);
+            if (current.right) stack.push(current.right);
+
+		}
+
+        return lowest;
 
     }
 
@@ -73,7 +169,46 @@ class BinaryTree {
      * (i.e. are at the same level but have different parents. ) */
 
     areCousins(node1, node2) {
+        const node = this.root;
+        const answers = [];
 
+        if ([node1.val, node2.val].includes(node.val)) {
+            return false;
+        }
+
+        function getDepth(node, count) {
+            let left;
+            let right;
+            let parent = node;
+
+            if ([node1.val, node2.val].includes(node.val)) {
+                return [count, parent];
+            } else {
+                count++;
+            }
+            if (node.left) {
+               left = getDepth(node.left, count);
+               if (left) {
+                answers.push(left);
+               }
+            }
+
+            if (node.right) {
+               right = getDepth(node.right, count);
+               if (right) {
+                answers.push(right);
+               }
+            }
+
+        }
+
+        getDepth(node, 1);
+
+        if (answers.length === 2) {
+            return answers[0][0] !== answers[1][0] && answers[0][1] !== answers[1][1];
+        } else {
+            return "one or both node values not found in binary tree";
+        }
     }
 
     /** Further study!
@@ -99,8 +234,33 @@ class BinaryTree {
     }
 }
 
-const bt = new BinaryTree(new BinaryTreeNode(25, new BinaryTreeNode(30, new BinaryTreeNode(80), new BinaryTreeNode(100)), new BinaryTreeNode(35)));
+//const bt = new BinaryTree(new BinaryTreeNode(25, new BinaryTreeNode(30, new BinaryTreeNode(80), new BinaryTreeNode(100)), new BinaryTreeNode(35)));
 //console.log(bt);
-console.log(bt.minDepth());
+const bt = new BinaryTree();
+const headNode = new BinaryTreeNode(101010);
+bt.root = headNode;
+
+const headLeft = new BinaryTreeNode(2);
+headNode.left = headLeft;
+const headRight = new BinaryTreeNode(11);
+headNode.right = headRight;
+
+const node1 = new BinaryTreeNode(9);
+headLeft.left = node1;
+const node2 = new BinaryTreeNode(10);
+headLeft.right = node2;
+
+const node3 = new BinaryTreeNode(6);
+const node4 = new BinaryTreeNode(12);
+headRight.left = node3;
+headRight.right = node4;
+
+node3.left = new BinaryTreeNode(7);
+node4.left = new BinaryTreeNode(8);
+node4.right = new BinaryTreeNode(13, new BinaryTreeNode(14), new BinaryTreeNode(15, new BinaryTreeNode(20)));
+
+
+//console.log(bt.nextLarger(5));
+console.log(bt.areCousins(new BinaryTreeNode(9), new BinaryTreeNode(13)));
 
 module.exports = { BinaryTree, BinaryTreeNode };
